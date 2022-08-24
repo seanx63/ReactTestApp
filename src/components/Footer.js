ddd35735324d22e247a769c72c39f01833001487
Sean Gates <seanx63@gmail.com>
2022-08-24 12:03:09 -0700

    init

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        Alice in Wonderland by Lewis Carrol as well as the original
+        illustrations by John Tenniel are in the public domain.<br></br>
+        Text grabbed from Project Gutenberg. Illustration grabbed from{" "}
+        <a href="www.alice-in-wonderland.net">www.alice-in-wonderland.net</a>.
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../App.css';
+
+const Header = () => {
+  return (
+    <div><h1>Alice in Wonderland by Lewis Carrol</h1></div>
+  )
+}
+
+export default Header
